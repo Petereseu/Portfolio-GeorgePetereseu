647a005f255d6eb65db36759836a185a039f9ff7
its_Pete <108576383+Petereseu@users.noreply.github.com>
2026-02-16 14:06:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function toggleTheme() {
     document.body.classList.toggle("light");
 }
+// Scroll reveal animation
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+}, { threshold: 0.15 });
+
+sections.forEach(section => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
+
